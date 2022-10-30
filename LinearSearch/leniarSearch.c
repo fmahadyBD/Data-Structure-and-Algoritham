0f86162c43e04bd74b1683a74a468f1b823cee3c
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-10-30 09:18:25 +0600

    Add files via upload

---
+++ LinearSearch/leniarSearch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int arr[6] = {3, 5, 3, 1, 27, 8};
+    int i, a,c=0;
+     printf("Seach the element: ");
+    scanf("%d", &a);
+    for (i = 0; i < 6; i++)
+    {
+        if (arr[i] == a)
+        {
+             printf("Succesfuly find it!");
+            return 0;
+        }
+        else
+        {
+           c++;
+        }
+    }
+    if(c>=6){
+         printf("Sorry not found");
+    }
+}
