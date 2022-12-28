a29300110cfafe76e66cba488976c52f8897dd63
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-12-28 23:46:44 +0600

    Update countingSort.c

--- Countsort/countingSort.c
+++ Countsort/countingSort.c
@@ -10,18 +10,18 @@ int main()
 
     printf("Enter the array size: \n");
 
-    scanf("%d", &size);
+    scanf("%d", &size);// we wnat to know about our array size
     int a[size];
     int b[size];
     printf("Enter the array Element:\n");
     for (int i = 0; i < size; i++)
     {
 
-        scanf("%d", &a[i]);
+        scanf("%d", &a[i]);// we take the input of array that we want to sorting
     }
 
 
-
+    /*In here we cheeck every number and find the maximum numberof our array. It will help to us to find the our counting array size*/
     for (int i = 0; i < size; i++)
     {
         if (max < a[i])
@@ -30,8 +30,9 @@ int main()
         }
     }
 
-    int c[max + 1];
-
+    /*We take the max valu and dlecar our array. That will count our main array element how number that stay in the main array*/
+    int c[max + 1]
+    //we assing all value is 0; cause we need to count it
     for (int i = 0; i < max; i++)
     {
 
@@ -43,6 +44,7 @@ int main()
 
         int temp = a[i];
         c[temp] = c[temp] + 1;
+        //We count the number
  
     }
  /*We need to 3 array for sorting our data, !st is main array that we want to sort.. 2nd ei count the poision of numbar
@@ -61,6 +63,7 @@ int main()
             {
                 printf("%d ", i);
                 --c[i];
+                // we print the position of the array. cause you store the position of the element and count it, and here we print the position
             }
         }
     }
